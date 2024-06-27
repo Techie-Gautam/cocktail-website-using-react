73bc532e4ca172ade63a6779b4ec84d15609ac71
Techie-Gautam <gautam.techie4803@gmail.com>
2024-06-27 15:19:22 +0530

    initial commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni cumque illo ab placeat optio saepe blanditiis eos. Sed voluptas laborum pariatur voluptatum mollitia excepturi quos animi nisi dolorem sequi voluptatibus porro ea, labore ipsa? Ipsam corrupti iure consequatur eius ipsa.
+      </p>
+    </section>
+  )
+}
+
+export default About;
